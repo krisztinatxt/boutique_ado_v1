03e1432c214d9500a50dae95463b64258027ce24
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-06-15 18:50:14 +0000

    Allauth added, email verfication works

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,5 +1,4 @@
 """boutique_ado URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -18,6 +17,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('allauth.urls')),
-
+    path('accounts/', include('allauth.urls')),
 ]
